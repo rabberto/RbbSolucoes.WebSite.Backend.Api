1febd114117460e57c4ace4d42b64de7b042b8bf
rabberto <rafel.rbberto@gmail.com>
2025-10-09 17:22:49 -0300

    Removendo variaveis do .yml

--- src/Api/Configurations/DatabaseConfiguration.cs
+++ src/Api/Configurations/DatabaseConfiguration.cs
@@ -14,9 +14,6 @@ namespace RbbSolucoes.Website.Backend.Api.Configurations;
 [ExcludeFromCodeCoverage]
 public static class DatabaseConfiguration
 {
-    private readonly static string _connectionString = AppSettings.Settings?.MongoDbSettings?.ConnectionString
-        ?? throw new InvalidOperationException("MongoDbSettings.ConnectionString is not initialized.");
-
     public static IServiceCollection AddDatabaseConfiguration(this IServiceCollection services)
     {
         string connectionString = AppSettings.Settings?.MongoDbSettings?.ConnectionString 
